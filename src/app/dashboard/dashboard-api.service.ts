9d6bd0936bfe557a167deff9a801509fb0c202fa
SethOfCarso <cardona.erick@hotmail.com>
2020-11-25 17:08:10 -0600

    Done with iot icons , oxigen, temp

--- src/app/dashboard/dashboard-api.service.ts
+++ src/app/dashboard/dashboard-api.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardAPIService {
 
-  constructor() { }
+  newURL:string;
+  urlGETTemperature = 'https://industrial.api.ubidots.com/api/v1.6/variables/5fbc7e7673efc35da4ff3329/values';
+  urlGETOxigene = 'https://industrial.api.ubidots.com/api/v1.6/variables/5fbc7e750ff4c31c331fb8ea/values';
+  token = 'BBFF-8XRqQNbNsBTUAX9RDgeTms4LfhMKex';
+
+  valTemp1:any = [];
+  valTemp1Subject = new BehaviorSubject<[]>([]);
+
+  valOxi1:any =[];
+  valOxi1Subject = new BehaviorSubject<[]>([]);
+
+  constructor(private http: HttpClient) {
+
+   }
+
+   readValTemp1(){
+    return this.valTemp1.slice();
+  }
+
+  readValOxi1(){
+    return this.valOxi1.slice();
+  }
+
+  //  ===========================================
+  //  Get usuarios from server
+  //  ===========================================
+  loadValTemperatura() {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type','application/json').set('X-Auth-Token', this.token);
+    this.http.get(this.urlGETTemperature,{headers: headers}).subscribe(
+      (data:any) => {
+        // console.log("Temperatura");
+        // console.log(data.results);
+        this.valTemp1 = data.results;
+        this.valTemp1Subject.next(this.readValTemp1());
+        // this.answSubject.next('Se cargaron los usuarios correctamente');
+      },
+      (err) => (console.log(err))
+    );
+  }
+
+  loadValOxigeno() {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type','application/json').set('X-Auth-Token', this.token);
+    this.http.get(this.urlGETOxigene,{headers: headers}).subscribe(
+      (data:any) => {
+        // console.log("Oxigeno");
+        // console.log(data.results);
+          this.valOxi1 = data.results;
+        this.valOxi1Subject.next(this.readValOxi1());
+        
+        // this.answSubject.next('Se cargaron los usuarios correctamente');
+      },
+      (err) => (console.log(err))
+    );
+  }
+
 }
--- src/app/dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/dashboard/dashboard-main/dashboard-main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { DashboardAPIService } from '../dashboard-api.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-main',
@@ -9,41 +11,128 @@ import { Color, Label } from 'ng2-charts';
 })
 export class DashboardMainComponent implements OnInit {
 
-  
-
   now:number;
+  temperatureData=[{value:0,timestamp:1606257014436}, {value:0,timestamp:0}];
+  oxigenData=[{value:"0"},{value:0}];
+  bed1=0;
+  bed2=0;
+  bed3=0;
+  bed4=0;
+  bed5=0;
+  tempo=0;
+  oxig:any=0;
 
-    constructor() {
+  lastUpdate;
 
-      
+    constructor(private apiService:DashboardAPIService) {
+
+      this.udpateInfo();
+      this.lastUpdate = this.now;
+
+      //in 10 seconds do something
+      interval(5000).subscribe(x => {
+        // console.log("Llamo cada 5 segs");
+        this.udpateInfo();
+        this.lastUpdate = this.now;
+        this.updateBedStatus();
+      });
 
         setInterval(() => {
           this.now = Date.now();
         }, 1);
     }
+    
+    lineChartData: ChartDataSets[] = [
+      { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+    ];
+
+    lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+    lineChartOptions = {
+      responsive: true,
+    };
+
+    lineChartColors: Color[] = [
+      {
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,255,0,0.28)',
+      },
+    ];
+
+    lineChartLegend = true;
+    lineChartPlugins = [];
+    lineChartType = 'line';
+
+    updateBedStatus(){
+      this.tempo = this.temperatureData[0].value;
+      this.oxig = this.oxigenData[0].value;
+      console.log(this.tempo);
+      if((this.tempo >= 36 && this.tempo <37.5 ) && (this.oxig > 94) ){
+        this.bed1 = 1;
+        console.log("1");
+        
+      } 
+      if(this.tempo > 37.5 && this.tempo <= 38){
+        this.bed1 = 2;
+        console.log("2");
+        
+      }
+       if(this.oxig >= 90 && this.oxig <=94 ){
+        this.bed1 = 2;
+        console.log("4");
+      }
+
+      if(this.tempo > 38 || this.tempo < 36){
+        this.bed1 = 3;
+        console.log("3");
+      }
+       if(this.oxig < 90){
+        this.bed1 = 3;
+        console.log("5");
+      }
+      
+      console.log("Cama");
+      console.log(this.bed1);
+
+    }
+
+    udpateInfo(){
+      // console.log("Hola mundo");
+      this.apiService.loadValTemperatura();
+      
+      this.apiService.valTemp1Subject.subscribe((data:any)=>
+    {
+      if(data != undefined){
+        if(data[0] != undefined)
+        {
+        this.temperatureData = data;
+        // console.log(this.temperatureData);
+        } else{
+        console.log("Error");
+        }
+      }
+    })
+
+      this.apiService.loadValOxigeno();
+      this.apiService.valOxi1Subject.subscribe((data:any)=>
+    {
+      if(data[0] != undefined)
+      {
+        if(data[0] != undefined){
+          this.oxigenData = data;
+          // console.log(data);
+        // console.log(this.oxigenData);
+        }
+      }
+      else{
+        console.log("Error");
+      }
+      // this.loginFalse();
+    })
+    }
 
   ngOnInit(): void {
   }
-  lineChartData: ChartDataSets[] = [
-    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
-  ];
-
-  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
-
-  lineChartOptions = {
-    responsive: true,
-  };
-
-  lineChartColors: Color[] = [
-    {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,255,0,0.28)',
-    },
-  ];
-
-  lineChartLegend = true;
-  lineChartPlugins = [];
-  lineChartType = 'line';
 
 }
 
